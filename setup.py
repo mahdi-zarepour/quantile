4c178df395feb238393b458e8905de86fca7770b
mahdi-zarepour <mahdizarepour40@gmail.com>
2023-01-12 15:35:42 +0330

    repair names for upload package in pypi

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ with open("README.md", "r") as fh:
     description = fh.read()
   
 setuptools.setup(
-    name="quantile",
+    name="statistics_quantile",
     version="0.0.1",
     author="Mahdi-Zarepour",
     author_email="mahdizarepour40@gmail.com",
-    packages=["quantile"],
+    packages=["statistics_quantile"],
     description="quantile in Python",
     long_description=description,
     long_description_content_type="text/markdown",
